aab86dc42d122e4d3454521e28a4bcff40caa1d7
K Praveen Kumar <kpkcool@gmail.com>
2022-12-02 16:26:37 +0530

    Initial Commit

---
+++ Daily Task/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Daily Task
+//
+//  Created by K Praveen Kumar on 02/12/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
