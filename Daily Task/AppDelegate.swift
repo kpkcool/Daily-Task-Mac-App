406bdee5b9763982d5f6ff712a647ff7be7311f0
K Praveen Kumar <kpkcool@gmail.com>
2022-12-04 07:55:24 +0530

    daily task setup status bar

--- Daily Task/AppDelegate.swift
+++ Daily Task/AppDelegate.swift
@@ -10,11 +10,22 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    
-
+    var item : NSStatusItem? = nil
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        item = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        //item?.button?.title = "Daily Task"
+        item?.button?.image = NSImage(named: "DailyTodo")
+        
+        
+        let menu = NSMenu()
+        menu.addItem(withTitle: "Naukri", action: #selector(AppDelegate.OpenNaukri), keyEquivalent: "n")
+        menu.addItem(withTitle: "LinkedIn", action: #selector(AppDelegate.OpenLinkedIn), keyEquivalent: "l")
+        menu.addItem(withTitle: "Kibho", action: #selector(AppDelegate.OpenKibho), keyEquivalent: "k")
+        
+        item?.menu = menu
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -26,5 +37,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
 
+    @objc func OpenNaukri(){
+        //Open Naukri from Chrome
+    }
+    @objc func OpenLinkedIn(){
+        //Open LinkedIn from Chrome
+    }
+    @objc func OpenKibho(){
+        //Open Kibho from Chrome
+    }
 }
 
